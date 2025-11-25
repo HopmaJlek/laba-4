a90ccd812ad8039617e6a48533e58df310d7fa6c
HopmaJlek <145129346+HopmaJlek@users.noreply.github.com>
2025-11-25 10:42:44 +0300

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
  
 app = Flask(__name__) 
  
-DATABASE_URL = os.environ.get("DB_URL") 
+DATABASE_URL = os.environ.get("DATABASE_URL") 
  
 if DATABASE_URL: 
     url = urlparse(DATABASE_URL) 
