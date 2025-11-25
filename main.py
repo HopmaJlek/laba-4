41bb7e7108accfbf0dabf2397cc3a58e53cdf7d5
HopmaJlek <145129346+HopmaJlek@users.noreply.github.com>
2025-11-25 10:39:03 +0300

    main.py

--- main.py
+++ main.py
@@ -38,8 +38,7 @@ def save_message():
         return jsonify({"error": "Database not connected"}), 500 
  
     data = request.get_json()  
-    message = data.get("message", "") 
-    if data else "" 
+    message = data.get("message", "") if data else "" 
  
     with conn.cursor() as cur: 
         cur.execute(""" 
@@ -84,11 +83,11 @@ def hello():
 
 @app.route("/echo", methods=["POST"]) 
 def echo(): 
-data = request.get_json() 
-return jsonify({ 
+    data = request.get_json() 
+    return jsonify({ 
 "status": "received", 
 "you_data": data, 
 "length": len(str(data)) if data else 0 
 }) 
 if __name__ == "__main__": 
-app.run(host="0.0.0.0", port=5000) 
+    app.run(host="0.0.0.0", port=5000) 
